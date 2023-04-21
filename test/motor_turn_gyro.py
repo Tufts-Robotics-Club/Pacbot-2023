2bbda6ded98a085ef662f92fe57851e1a68dec79
Jo Chung <chjoanna@163.com>
2023-04-21 17:05:21 -0400

    Update motor_turn_gyro.py

--- test/motor_turn_gyro.py
+++ test/motor_turn_gyro.py
@@ -25,7 +25,7 @@ motor2.stop()
 
 angle = 0
 
-while angle > 90:
+while angle > -90:
     angle += sensor.gyro[0] * 57.29578
     print("Gyroscope (rad/sec): {}".format(sensor.gyro))
     print(sensor.gyro[0])
