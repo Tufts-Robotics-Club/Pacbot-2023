490c9fa109167d86071741263206c9d72ec85563
Jo Chung <chjoanna@163.com>
2023-04-21 15:43:21 -0400

    Add target angle variable

--- test/motor_turn_90.py
+++ test/motor_turn_90.py
@@ -2,13 +2,15 @@ from gpiozero import PhaseEnableMotor, RotaryEncoder
 from time import sleep
 
 MAX_ANGLE = 90
+START_ANGLE = sensor.euler[0]
+TARGET_ANGLE = sensor.euler[0] + MAX_ANGLE
 
 motor1 = PhaseEnableMotor(19, 26)
 motor2 = PhaseEnableMotor(6, 13)
 rotar1 = RotaryEncoder(23, 24, max_steps=0)
 rotar2 = RotaryEncoder(14, 15, max_steps=0)
 
-while format(sensor.euler) < MAX_ANGLE:
+while (sensor.euler[0] < TARGET_ANGLE):
     print("Euler angle: {}".format(sensor.euler))       
     motor1.forward(0.5)
     motor2.backward(0.5)
