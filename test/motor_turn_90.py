7f229d426c5fb7907cd55265484563bc2e72bad2
Jo Chung <chjoanna@163.com>
2023-04-21 15:26:45 -0400

    Create test code for tuning in place

---
+++ test/motor_turn_90.py
@@ -0,0 +1,21 @@
+from gpiozero import PhaseEnableMotor, RotaryEncoder
+from time import sleep
+
+MAX_ANGLE = 90
+
+motor1 = PhaseEnableMotor(19, 26)
+motor2 = PhaseEnableMotor(6, 13)
+rotar1 = RotaryEncoder(23, 24, max_steps=0)
+rotar2 = RotaryEncoder(14, 15, max_steps=0)
+
+while format(sensor.euler) < MAX_ANGLE:
+    print("Euler angle: {}".format(sensor.euler))       
+    motor1.forward(0.5)
+    motor2.backward(0.5)
+    sleep(0.1)
+
+motor1.stop()
+motor2.stop()
+
+#     print(rotar1.steps, rotar2.steps)
+
