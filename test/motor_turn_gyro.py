dc7e340b248ad4a2996812c53ec7488cf5d49a1e
Jo Chung <chjoanna@163.com>
2023-04-21 16:10:21 -0400

    Create motor_turn_gyro.py

---
+++ test/motor_turn_gyro.py
@@ -0,0 +1,18 @@
+from gpiozero import PhaseEnableMotor, RotaryEncoder
+from time import sleep
+
+motor1 = PhaseEnableMotor(19, 26)
+motor2 = PhaseEnableMotor(6, 13)
+rotar1 = RotaryEncoder(23, 24, max_steps=0)
+rotar2 = RotaryEncoder(14, 15, max_steps=0)
+motor1.forward(0.5)
+motor2.forward(0.5)
+motor1.stop()
+motor2.stop()
+
+
+
+while True:
+    print("Gyroscope (rad/sec): {}".format(sensor.gyro))
+    print(rotar1.steps, rotar2.steps)
+    sleep(0.1)
