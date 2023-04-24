c6f5fc760864cfe8a46d57504beb189d82ee2abe
Jo Chung <chjoanna@163.com>
2023-04-23 21:07:44 -0400

    Update motor_test.py

--- test/motor_test.py
+++ test/motor_test.py
@@ -5,8 +5,8 @@ motor1 = PhaseEnableMotor(22, 27)
 motor2 = PhaseEnableMotor(5, 6)
 rotar1 = RotaryEncoder(23, 24, max_steps=0)
 rotar2 = RotaryEncoder(14, 15, max_steps=0)
-motor1.forward(0.5)
-motor2.forward(0.5)
+motor1.forward(0.1)
+motor2.forward(0.1)
 
 while True:
     print(rotar1.steps, rotar2.steps)
