6ff9fe4de75e584ba3b399bbf1359064c6ca0681
Jo Chung <chjoanna@163.com>
2023-04-23 21:07:29 -0400

    Update motor_test.py

--- test/motor_test.py
+++ test/motor_test.py
@@ -1,7 +1,7 @@
 from gpiozero import PhaseEnableMotor, RotaryEncoder
 from time import sleep
 
-motor1 = PhaseEnableMotor(19, 26)
+motor1 = PhaseEnableMotor(22, 27)
 motor2 = PhaseEnableMotor(5, 6)
 rotar1 = RotaryEncoder(23, 24, max_steps=0)
 rotar2 = RotaryEncoder(14, 15, max_steps=0)
