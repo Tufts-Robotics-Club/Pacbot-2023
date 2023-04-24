ba1d6b9ff6a3e72ff97190f2e783a86f1351fdf6
Jo Chung <chjoanna@163.com>
2023-04-24 19:21:46 -0400

    Update motor_turn_90.py

--- test/motor_turn_90.py
+++ test/motor_turn_90.py
@@ -48,8 +48,8 @@ while (DELTA < TARGET_ANGLE):
         DELTA = sensor.euler[0]
 
     print("Current Euler angle: {}".format(sensor.euler))       
-    motor2.forward(0.1)
-    motor1.backward(0.1)
+    motor1.forward(0.1)
+    motor2.backward(0.1)
     sleep(0.1)
 
 motor1.stop()
