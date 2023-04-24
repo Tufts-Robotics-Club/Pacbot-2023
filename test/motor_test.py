f9da4dd54c4287b91d4782184c5c5302eb7f0a3c
Jo Chung <chjoanna@163.com>
2023-04-23 21:17:09 -0400

    Changed pin

--- test/motor_test.py
+++ test/motor_test.py
@@ -1,12 +1,12 @@
 from gpiozero import PhaseEnableMotor, RotaryEncoder
 from time import sleep
 
-motor1 = PhaseEnableMotor(22, 27)
+motor1 = PhaseEnableMotor(19, 26)
 motor2 = PhaseEnableMotor(5, 6)
 rotar1 = RotaryEncoder(23, 24, max_steps=0)
 rotar2 = RotaryEncoder(14, 15, max_steps=0)
-motor1.forward(0.1)
-motor2.forward(0.1)
+motor1.forward(0.5)
+motor2.forward(0.5)
 
 while True:
     print(rotar1.steps, rotar2.steps)
