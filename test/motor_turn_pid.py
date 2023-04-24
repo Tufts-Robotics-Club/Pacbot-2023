0e45b43d9db0b699b8751df854956852fd0eac9a
Jo Chung <chjoanna@163.com>
2023-04-23 20:26:31 -0400

    Changed test code for turning

--- test/motor_turn_pid.py
+++ test/motor_turn_pid.py
@@ -4,6 +4,7 @@ import time
 import board
 import adafruit_bno055
 import adafruit_tca9548a
+from simple_pid import PID
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 
@@ -13,10 +14,7 @@ tca = adafruit_tca9548a.TCA9548A(i2c)
 # imu (attached to port 0 of multiplexer)
 sensor = adafruit_bno055.BNO055_I2C(tca[0])
 
-motor1 = PhaseEnableMotor(19, 26)
-motor2 = PhaseEnableMotor(5, 6)
-rotar1 = RotaryEncoder(23, 24, max_steps=0)
-rotar2 = RotaryEncoder(14, 15, max_steps=0)
+pid = PID(1, 0.1, 0.05, setpoint=1)
 
 MAX_ANGLE = 90
 START_ANGLE = sensor.euler[0]
@@ -24,7 +22,12 @@ print("Start Euler angle: {}".format(sensor.euler))
 TARGET_ANGLE = sensor.euler[0] + MAX_ANGLE
 print(TARGET_ANGLE)       
 
-while (sensor.euler[0] < TARGET_ANGLE):
+motor1 = PhaseEnableMotor(19, 26)
+motor2 = PhaseEnableMotor(5, 6)
+rotar1 = RotaryEncoder(23, 24, max_steps=0)
+rotar2 = RotaryEncoder(14, 15, max_steps=0)
+
+while true:
     print("Current Euler angle: {}".format(sensor.euler))       
     motor1.forward(0.5)
     motor2.backward(0.5)
