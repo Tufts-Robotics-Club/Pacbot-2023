12d419237e6d314432db7f841ad2954355ac316c
Jo Chung <chjoanna@163.com>
2023-04-23 20:41:47 -0400

    Update motor_turn_90.py

--- test/motor_turn_90.py
+++ test/motor_turn_90.py
@@ -15,8 +15,8 @@ sensor = adafruit_bno055.BNO055_I2C(tca[0])
 
 motor1 = PhaseEnableMotor(19, 26)
 motor2 = PhaseEnableMotor(5, 6)
-rotar1 = RotaryEncoder(23, 24, max_steps=12)
-rotar2 = RotaryEncoder(14, 15, max_steps=12)
+rotar1 = RotaryEncoder(23, 24, max_steps=0)
+rotar2 = RotaryEncoder(14, 15, max_steps=0)
 
 TURN_ANGLE = 90
 START_ANGLE = sensor.euler[0]
@@ -26,8 +26,8 @@ print(TARGET_ANGLE)
 
 while (sensor.euler[0] < TARGET_ANGLE):
     print("Current Euler angle: {}".format(sensor.euler))       
-    motor2.forward(0.1)
-    motor1.backward(0.1)
+    motor2.forward(0.05)
+    motor1.backward(0.05)
     sleep(0.1)
 
 motor1.stop()
