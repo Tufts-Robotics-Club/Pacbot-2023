437ee68ffeea8642fb3369e9b28a2e20feef6881
Tubby101 <gckyoshi@gmail.com>
2023-04-29 02:01:47 -0400

    updating pins

--- test/motor_test.py
+++ test/motor_test.py
@@ -2,7 +2,7 @@ from gpiozero import PhaseEnableMotor
 import Encoder
 from time import sleep
 
-motor1 = PhaseEnableMotor(22, 27)
+motor1 = PhaseEnableMotor(23, 24)
 motor2 = PhaseEnableMotor(6, 5)
 rotar1 = Encoder.Encoder(23, 24)
 rotar2 = Encoder.Encoder(17, 25)
@@ -12,8 +12,8 @@ rotar2 = Encoder.Encoder(17, 25)
 counter = 0
 while counter < 100:
     print(rotar1.read(), rotar2.read())
-    motor1.backward(0.1)
-    motor2.backward(0.1)
+    motor1.backward(0.5)
+    motor2.backward(0.5)
     counter += 1
     sleep(0.05)
 
